9b0ffaee18a70c53b0d67dc9f89a682fb367f389
HarshPanchal26 <hrppanchal27@gmail.com>
2025-01-29 18:14:54 +0530

    controller for class is been added

--- app/controllers/ClassesController.ts
+++ app/controllers/ClassesController.ts
@@ -1,51 +1,20 @@
 import type { HttpContext } from '@adonisjs/core/http'
-import classes from '#models/classes'
-import { CreateValidatorForclasses, UpdateValidatorForclasses } from '#validators/classes'
+import Classes from '#models/Classes'
+import { CreateValidatorForClasses } from '#validators/Classes'
 
 
 export default class ClassesController {
-  
-  async index(ctx: HttpContext) {
-    const classes = await classes.query().paginate(ctx.request.input('page', 1), 10);
-    return ctx.response.json(classes.serialize());
+
+  async createClass(ctx: HttpContext) {
+
+    let school_id = ctx.auth.user?.school_id
+    if (ctx.auth.user?.role_id !== 1) {
+      return ctx.response.status(403).json({ message: 'You are not allocated to manage this functions.' });
+    }
+    const payload = await CreateValidatorForClasses.validate(ctx.request.body());
+    const created_class = await Classes.create({ ...payload, school_id: school_id });
+    return ctx.response.json(created_class.serialize());
   }
-
-  async create(ctx: HttpContext) {
-    const payload = await CreateValidatorForclasses.validate(ctx.request.all());
-    const classes = await classes.create(payload);
-    return ctx.response.json(classes.serialize());
-  }
-
-  async store(ctx: HttpContext) { }
-
-  async show(ctx: HttpContext) {
-    const classes = await classes.findOrFail(ctx.request.input('id'));
-    return ctx.response.json(classes.serialize());
-  }
-
-  async update(ctx: HttpContext) {
-    const payload = await UpdateValidatorForclasses.validate(ctx.request.all());
-    const classes = await classes.findOrFail(ctx.request.input('id'));
-    await classes.merge(payload).save();
-    return ctx.response.json(classes.serialize());
-  }
-
-  async filterclasses(ctx: HttpContext) {
-    const classes = await classes.query().where('role', 'student').paginate(ctx.request.input('page', 1), 10);
-    return ctx.response.json(classes.serialize());
-  }
-
-  // async destroy(ctx: HttpContext) {
-  //   const classes = await classes.findOrFail(ctx.request.input('id'));
-  //   await user.delete();
-  // }
-
-  // async edit(ctx: HttpContext) {
-  //   const payload = await UpdateValidatorForclasses.validate(ctx.request.all());
-  //   const user = await classes.findOrFail(ctx.request.input('id'));
-  //   user.merge(payload).save();
-  //   return ctx.response.json(user.serialize());
-  // }
 }
 
 
