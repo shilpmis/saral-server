572b2eba005efb0386e4cc91a82018a89ed93fe8
HarshPanchal26 <hrppanchal27@gmail.com>
2025-02-17 10:09:49 +0530

    updated routes

--- start/routes.ts
+++ start/routes.ts
@@ -15,6 +15,8 @@ import StaffController from '#controllers/StaffMasterController'
 import UsersController from '#controllers/UsersController'
 import StundetsController from '#controllers/StundetsController'
 import StaffMasterController from '#controllers/StaffMasterController'
+import SchoolsController from '#controllers/SchoolsController'
+import TeachersController from '#controllers/TeachersController'
 
 router.get('/', async () => {
   return {
@@ -33,13 +35,16 @@ router.group(() => {
   
   router.get('/verify', [AuthController , 'verifyUser'])
   router.get('/logout', [AuthController , 'logout'])
+
+  router.get('/school/:school_id' , [SchoolsController , 'index'])
+  router.put('/school/:school_id' , [SchoolsController , 'update'])
   
-  router.get('/user/:school_id', [UsersController , 'indexSchoolUsers'])
+  router.get('/users/:school_id', [UsersController , 'indexSchoolUsers'])
   router.post('/user', [UsersController , 'createUser'])
   router.put('/user/:school_id/:user_id', [UsersController , 'update'])
   
   router.get('/classes/:school_id', [ClassesController , 'indexClassesForSchool']);
-  router.post('/class', [ClassesController , 'createClass']);
+  router.post('/class/base', [ClassesController , 'createClass']);
   router.post('/classes', [ClassesController , 'createMultipleClasses']);
   router.post('/class/division', [ClassesController , 'createDivision']);
   router.put('/class/:class_id', [ClassesController , 'updateClass']);
@@ -48,16 +53,25 @@ router.group(() => {
   router.post('students/:class_id' , [StundetsController , 'createStudents']);
   router.put('students/:class_id' , [StundetsController , 'updateStudents']);
 
-  router.get('/staff-master/:school_id', [StaffMasterController , 'indexStaffMasterForSchool']);
-  router.post('/staff-master', [StaffMasterController , 'createStaffMasterRole']);
-  router.put('/staff-master/:id', [StaffMasterController , 'updateStaffMasterRole']);
-  router.delete('/staff-master/:id', [StaffMasterController , 'deleteStaffMasterRole']);
+  router.get('/staff/:school_id', [StaffMasterController , 'indexStaffMasterForSchool']);
+  router.post('/staff', [StaffMasterController , 'createStaffRole']);
+  router.put('/staff/:id', [StaffMasterController , 'updateStaffRole']);
+  router.delete('/staff/:id', [StaffMasterController , 'deleteStaffRole']);
 
-  router.post('/users', '#controllers/users_controller.store')
-  router.get('/users', '#controllers/users_controller.index')
-  router.get('/users/:id', '#controllers/users_controller.show')
-  router.put('/users/:id', '#controllers/users_controller.update')
-  router.delete('/users/:id', '#controllers/users_controller.destroy')
+  /**
+   * need think of for adding school_id in teachers creation
+   */
+  router.get('teachers/:school_id' , [TeachersController , 'indexTeachersForSchool'])
+  router.post('teachers/:school_id' , [TeachersController , 'createTeacher'])
+  router.put('teacher/:school_id/:teacher_id' , [TeachersController , 'updateTeacher'])
+
+
+
+  // router.post('/users', [UsersController , 'createUser'])
+  // router.get('/users', [UsersController , 'createUser'])
+  // router.get('/users/:id', '#controllers/users_controller.show')
+  // router.put('/users/:id', '#controllers/users_controller.update')
+  // router.delete('/users/:id', '#controllers/users_controller.destroy')
 
 }).prefix('/api/v1/').use(middleware.auth())
 
