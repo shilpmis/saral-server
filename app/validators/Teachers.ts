0ab4d1d2ad873be70ea653a4184add48a3055120
Prashant Kumar <prashantkrmathur@gmail.com>
2025-02-26 15:48:05 +0530

    aaded api for uploading teacher data using excel sheet with csv

--- app/validators/Teachers.ts
+++ app/validators/Teachers.ts
@@ -87,3 +87,58 @@ export const UpdateValidatorForTeachers = vine.compile(
   })
 )
 
+export const CreateTeacherValidator = vine.compile(
+    vine.object({
+      // add here
+      staff_role_id: vine.number(),
+      first_name: vine.string().trim().minLength(2).maxLength(50),
+      middle_name: vine.string().trim().minLength(2).maxLength(50),
+      last_name: vine.string().trim().minLength(2).maxLength(50),
+
+      first_name_in_guj: vine.string().trim().minLength(2).maxLength(50),
+      middle_name_in_guj: vine.string().trim().minLength(2).maxLength(50),
+      last_name_in_guj: vine.string().trim().minLength(2).maxLength(50),
+
+      aadhar_no: vine.number()
+        .unique({ table: 'teachers', column: 'aadhar_no' }),
+
+      religiion: vine.string().trim().minLength(2).maxLength(50),
+      religiion_in_guj: vine.string().trim().optional(),
+  
+      caste: vine.string().trim().minLength(2).maxLength(50),
+      caste_in_guj: vine.string().trim().optional(),
+  
+      category: vine.enum(['ST', 'SC', 'OBC', 'OPEN']),
+
+      address: vine.string().trim().minLength(5).maxLength(200),
+  
+      district: vine.string().trim().minLength(3).maxLength(100),
+      city: vine.string().trim().minLength(3).maxLength(100),
+  
+      state: vine.string().trim().minLength(3).maxLength(50),
+  
+      postal_code: vine.number(),
+      
+      bank_name: vine.string().trim(),
+      
+      account_no: vine.number().positive(),
+  
+      IFSC_code: vine.string().trim(),
+
+      gender: vine.enum(['Male', 'Female']),
+
+      birth_date: vine.date(),
+
+      mobile_number: vine.number(),
+      email: vine.string().email(),
+
+      class_id: vine.number(),
+
+      qualification: vine.string(),
+      joining_date: vine.date(),
+
+      employment_status: vine.enum(['Permanent', 'Trial_period', 'Resigned', 'Contact_base', 'Notice_Period']),
+
+    })
+  // )
+)
