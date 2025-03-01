159a6e61ec6345bcf17a25253d0e9791bef026f8
Bhavik1363 <bhavik1363@gmail.com>
2025-03-01 14:23:45 +0530

    make some minor changes in database.ts and package.json file for server deployment

--- config/database.ts
+++ config/database.ts
@@ -7,11 +7,11 @@ const dbConfig = defineConfig({
     mysql: {
       client: 'mysql2',
       connection: {
-        host: env.get('DB_HOST'),
-        port: env.get('DB_PORT'),
-        user: env.get('DB_USER'),
-        password: env.get('DB_PASSWORD'),
-        database: env.get('DB_DATABASE'),
+        host: process.env.DB_HOST || env.get('DB_HOST'),
+        port: process.env.DB_PORT ? Number(process.env.DB_PORT) : env.get('DB_PORT'),
+        user: process.env.DB_USER || env.get('DB_USER'),
+        password: process.env.DB_PASSWORD || env.get('DB_PASSWORD'),
+        database: process.env.DB_DATABASE || env.get('DB_DATABASE'),
       },
       migrations: {
         naturalSort: true,
