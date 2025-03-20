db0df054af7a31dfdeffb22e6a5db186ff14fd94
Prashant Kumar <prashantkrmathur@gmail.com>
2025-03-20 22:03:15 +0530

    admission enquiry  table key status change : added enum value "interview scheduled and rejected ."

---
+++ database/migrations/1742487028667_create_admission_inquiries_table.ts
@@ -0,0 +1,34 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'admission_inquiries'
+
+  async up() {
+    this.schema.alterTable(this.tableName, (table) => {
+
+      table.enum("status", [
+        'pending', 
+        'eligible', 
+        'approved', 
+        'ineligible', 
+        'rejected', 
+        'interview scheduled'
+      ])
+      .defaultTo('pending')
+      .alter()
+    })
+  }
+
+  async down() {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.enum("status", [
+        'pending', 
+        'eligible', 
+        'approved', 
+        'ineligible'
+      ])
+      .defaultTo('pending')
+      .alter()
+    })
+  }
+}
