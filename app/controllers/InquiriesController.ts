839f3e724a1ada58c307d2c5eefbff8b4cfafe58
JayshreeERP <jayshree@melzo.com>
2025-04-08 17:50:05 +0530

    modified all end points

--- app/controllers/InquiriesController.ts
+++ app/controllers/InquiriesController.ts
@@ -4,7 +4,12 @@ import Students from '#models/Students'
 import StudentEnrollments from '#models/StudentEnrollments'
 import Quota from '#models/Quota'
 import db from '@adonisjs/lucid/services/db'
-
+import { CreateValidatorForInquiry, UpdateValidatorForInquiry } from '#validators/Inquiry'
+import AcademicSession from '#models/AcademicSession'
+import ClassSeatAvailability from '#models/ClassSeatAvailability'
+import { CreateValidatorStundet } from '#validators/Stundets'
+import QuotaAllocation from '#models/QuotaAllocation'
+import StudentMeta from '#models/StudentMeta'
 export default class InquiriesController {
   /**
    * Fetch paginated admission inquiries for the logged-in user's school
@@ -12,11 +17,17 @@ export default class InquiriesController {
   async listAllInquiries(ctx: HttpContext) {
     const school_id = ctx.auth.user!.school_id
     const page = ctx.request.input('page', 1)
-
+    const academic_session_id = ctx.request.input('academic_session', null)
+    if (!academic_session_id) {
+      return ctx.response.status(400).json({
+        message: 'Academic session ID is required',
+      })
+    }
     const inquiries = await AdmissionInquiry.query()
       .where('school_id', school_id)
+      .andWhere('academic_session_id', academic_session_id)
       .orderBy('created_at', 'desc')
-      .paginate(page, 6)
+      .paginate(page, 10)
 
     return ctx.response.status(200).json(inquiries)
   }
@@ -26,12 +37,63 @@ export default class InquiriesController {
    */
   async addInquiryForClass(ctx: HttpContext) {
     try {
-      const payload = ctx.request.all()
-      //  console.log("payload",payload);
+      // Validate the payload using the CreateValidatorForInquiry validator
+      const payload = await CreateValidatorForInquiry.validate(ctx.request.all())
+
+      let academicSession = await AcademicSession.query()
+        .where('id', payload.academic_session_id)
+        .andWhere('school_id', ctx.auth.user!.school_id)
+        .first()
+
+      if (!academicSession) {
+        return ctx.response.status(400).json({
+          message: 'Current academic session not found',
+        })
+      }
+
+      let classSeatAvailability = await ClassSeatAvailability.query()
+        .where('class_id', payload.inquiry_for_class)
+        .andWhere('academic_session_id', payload.academic_session_id)
+        .first()
+
+      if (!classSeatAvailability) {
+        return ctx.response.status(400).json({
+          message:
+            'Seat availability is not yet define for this class by admin for selected academic session',
+        })
+      }
+
+      if (payload.applying_for_quota) {
+        if (payload.quota_type === null) {
+          return ctx.response.status(400).json({
+            message: 'Quota type is required when applying for a quota',
+          })
+        }
+
+        const quota = await Quota.query()
+          .where('id', payload.quota_type)
+          .andWhere('academic_session_id', payload.academic_session_id)
+          .andWhere('school_id', ctx.auth.user!.school_id)
+          .first()
+
+        if (!quota) {
+          return ctx.response.status(400).json({
+            message: 'Quota not found',
+          })
+        }
+      }
+
+      if (!payload.applying_for_quota && payload.quota_type) {
+        return ctx.response.status(400).json({
+          message: 'Quota type should be null when not applying for a quota',
+        })
+      }
+
       const inquiry = await AdmissionInquiry.create({
         ...payload,
         created_by: ctx.auth.user!.id,
         school_id: ctx.auth.user!.school_id,
+        // class_applying_for : payload.inquiry_for_class,
       })
 
       return ctx.response.status(201).json({
@@ -39,11 +101,58 @@ export default class InquiriesController {
         inquiry,
       })
     } catch (error) {
+      console.log('Error adding inquiry:', error)
       return ctx.response.status(400).json({
         message: 'Error adding inquiry',
-        error: error.message,
+        error: error,
       })
     }
+  }
+
+  /**
+   * Update an admission inquiry
+   */
+  async updateInquiry(ctx: HttpContext) {
+    const payload = await UpdateValidatorForInquiry.validate(ctx.request.all())
+
+    const created_inquiry = await AdmissionInquiry.query()
+      .where('id', ctx.params.id)
+      .andWhere('school_id', ctx.auth.user!.school_id)
+      .first()
+
+    if (!created_inquiry) {
+      return ctx.response.status(404).json({
+        message: 'Inquiry not found',
+      })
+    }
+
+    if (payload.inquiry_for_class) {
+      let classSeatAvailability = await ClassSeatAvailability.query()
+        .where('class_id', payload.inquiry_for_class)
+        .andWhere('academic_session_id', created_inquiry.academic_session_id)
+        .first()
+
+      if (!classSeatAvailability) {
+        return ctx.response.status(400).json({
+          message:
+            'Seat availability is not yet define for this class by admin for selected academic session',
+        })
+      }
+    }
+
+    if (payload.status && created_inquiry.is_converted_to_student) {
+      return ctx.response.status(400).json({
+        message: 'This inquiry has already been converted into a student.',
+      })
+    }
+
+    created_inquiry.merge(payload)
+    await created_inquiry.save()
+
+    return ctx.response.status(200).json({
+      message: 'Admission inquiry updated successfully',
+      created_inquiry,
+    })
   }
 
   /**
@@ -51,50 +160,44 @@ export default class InquiriesController {
    */
   async getInquiryById(ctx: HttpContext) {
     const inquiry = await AdmissionInquiry.findOrFail(ctx.params.id)
-
     return ctx.response.status(200).json(inquiry)
-  }
-
-  /**
-   * Update an admission inquiry
-   */
-  async updateInquiry(ctx: HttpContext) {
-    const payload = ctx.request.all()
-
-    const inquiry = await AdmissionInquiry.findOrFail(ctx.params.id)
-    inquiry.merge(payload)
-    await inquiry.save()
-
-    return ctx.response.status(200).json({
-      message: 'Admission inquiry updated successfully',
-      inquiry,
-    })
   }
 
   /**
    * Delete an admission inquiry
    */
-  async destroy(ctx: HttpContext) {
-    const inquiry = await AdmissionInquiry.findOrFail(ctx.params.id)
-    await inquiry.delete()
+  // async destroy(ctx: HttpContext) {
+  //   const inquiry = await AdmissionInquiry.findOrFail(ctx.params.id)
+  //   await inquiry.delete()
 
-    return ctx.response.status(200).json({
-      message: 'Admission inquiry deleted successfully',
-    })
-  }
+  //   return ctx.response.status(200).json({
+  //     message: 'Admission inquiry deleted successfully',
+  //   })
+  // }
 
   /**
    * Convert an Admission Inquiry into a Student & Student Enrollment
    */
-  public async convertInquiryToStudent({ params, response }: HttpContext) {
+  public async convertInquiryToStudent(ctx: HttpContext) {
+    let school_id = ctx.auth.user!.school_id
+    let inquiry_id = ctx.params.inquiry_id
     const trx = await db.transaction()
 
     try {
       // Fetch Inquiry Data
-      const inquiry = await AdmissionInquiry.findOrFail(params.id)
+      const inquiry = await AdmissionInquiry.query()
+        .where('id', inquiry_id)
+        .andWhere('school_id', school_id)
+        .first()
+
+      if (!inquiry) {
+        return ctx.response.notFound({
+          error: 'Inquiry not found',
+        })
+      }
 
       if (inquiry.is_converted_to_student) {
-        return response.badRequest({
+        return ctx.response.badRequest({
           error: 'This inquiry has already been converted into a student.',
         })
       }
@@ -103,24 +206,40 @@ export default class InquiriesController {
       const admissionNumber = await this.generateAdmissionNumber()
       const enrollmentCode = `EN-${inquiry.school_id}-${Date.now()}`
 
+      const payload = await CreateValidatorStundet.validate(ctx.request.body())
+
       // Create Student Record
       const student = await Students.create(
         {
-          school_id: inquiry.school_id,
+          school_id: school_id,
           enrollment_code: enrollmentCode,
           admission_number: admissionNumber,
-          first_name: inquiry.first_name,
-          middle_name: '', // Can be updated later
-          last_name: '', // Can be updated later
-          gender: inquiry.gender === 'male' ? 'Male' : 'Female',
-          birth_date: inquiry.birth_date,
-          gr_no: await this.generateGrNo(),
-          primary_mobile: Number(inquiry.primary_mobile),
-          father_name: inquiry.father_name,
-          mother_name: '', // Can be updated later
-          roll_number: await this.generateRollNumber(inquiry.class_applying),
-          aadhar_no: undefined, // Can be updated later
+          gr_no: payload.students_data.gr_no,
+          first_name: payload.students_data.first_name,
+          middle_name: payload.students_data.middle_name,
+          last_name: payload.students_data.last_name,
+          first_name_in_guj: payload.students_data.first_name_in_guj,
+          middle_name_in_guj: payload.students_data.middle_name_in_guj,
+          last_name_in_guj: payload.students_data.last_name_in_guj,
+          gender: payload.students_data.gender,
+          birth_date: payload.students_data.birth_date,
+          primary_mobile: payload.students_data.primary_mobile,
+          father_name: payload.students_data.father_name,
+          father_name_in_guj: payload.students_data.father_name_in_guj,
+          mother_name: payload.students_data.mother_name,
+          mother_name_in_guj: payload.students_data.mother_name_in_guj,
+          roll_number: payload.students_data.roll_number,
+          aadhar_no: payload.students_data.aadhar_no,
           is_active: true,
+          // ...payload.students_data,
+        },
+        { client: trx }
+      )
+
+      await StudentMeta.create(
+        {
+          student_id: student.id,
+          ...payload.student_meta_data,
         },
         { client: trx }
       )
@@ -130,39 +249,70 @@ export default class InquiriesController {
 
       // Check if Quota is Available
       if (quotaId) {
-        const quota = await Quota.find(quotaId)
-        if (!quota) {
-          quotaId = null // Default to general category if quota is invalid
+        console.log('Check this here', inquiry.academic_session_id, quotaId)
+        const quota_allocation = await QuotaAllocation.query()
+          .where('quota_id', quotaId)
+          .andWhere('class_id', inquiry.inquiry_for_class)
+          .andWhere('academic_session_id', inquiry.academic_session_id)
+          .first()
+
+        if (!quota_allocation) {
+          return ctx.response.badRequest({
+            error: 'Quota Allocation for inquiry class is not yet defined by admin .',
+          })
         }
+
+        const clasSeatAvailability = await ClassSeatAvailability.query()
+          .where('class_id', inquiry.inquiry_for_class)
+          .andWhere('academic_session_id', inquiry.academic_session_id)
+          .first()
+
+        if (!clasSeatAvailability) {
+          return ctx.response.badRequest({
+            error: 'Class Seat Availability for inquiry class is not yet defined by admin .',
+          })
+        }
+
+        quota_allocation.filled_seats += 1
+        ;(await quota_allocation.save()).useTransaction(trx)
+
+        clasSeatAvailability.merge({
+          filled_seats: (clasSeatAvailability.filled_seats += 1),
+          quota_allocated_seats: (clasSeatAvailability.quota_allocated_seats -= 1),
+          remaining_seats: (clasSeatAvailability.remaining_seats -= 1),
+        })
+        ;(await clasSeatAvailability.save()).useTransaction(trx)
       }
 
       // Create Student Enrollment Record
       await StudentEnrollments.create(
         {
           student_id: student.id,
-          division_id: inquiry.class_applying,
+          division_id: inquiry.inquiry_for_class,
           academic_session_id: inquiry.academic_session_id,
-          quota_id: quotaId ?? undefined,
+          quota_id: quotaId ?? null,
           status: 'pursuing',
           remarks: 'Converted from Inquiry',
+          is_new_admission: true,
         },
         { client: trx }
       )
 
       // Mark Inquiry as Converted
       inquiry.is_converted_to_student = true
+      inquiry.status = 'enrolled'
       await inquiry.save()
 
       // Commit Transaction
       await trx.commit()
 
-      return response.status(201).json({
+      return ctx.response.status(201).json({
         message: 'Inquiry successfully converted into student record.',
         student,
       })
     } catch (error) {
       await trx.rollback()
-      return response.internalServerError({
+      return ctx.response.internalServerError({
         error: 'Error processing inquiry conversion',
         details: error.message,
       })
